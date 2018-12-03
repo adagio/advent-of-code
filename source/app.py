f67e0f2bcd4d813a8162772e4914ba9a060d543b
Franco Cedillo <franco.cedillo@gmail.com>
2018-12-03 18:40:34 -0500

    day03

--- source/app.py
+++ source/app.py
@@ -1,5 +1,7 @@
 #from modules.day01.driver import run as run01
-from modules.day02.driver import run as run02
+#from modules.day02.driver import run as run02
+from modules.day03.driver import run as run03
 
 #run01()
-run02()
+#run02()
+run03()
---
+++ source/tests/day03/test_parse.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+from modules.day03.parse import parse
+
+class ParseTestCase(TestCase):
+
+    def test_parse_example_1(self):
+        text = "#1 @ 3,2: 5x4"
+        claim_obj = {
+            "id": 1,
+            "left_edge": 3,
+            "top_edge": 2,
+            "width": 5,
+            "height": 4
+        }
+        self.assertEqual(parse(text), claim_obj)

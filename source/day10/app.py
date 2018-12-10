99b4435cf252b3a3cde1eb30775fe27619768cff
Franco Cedillo <franco.cedillo@gmail.com>
2018-12-10 17:49:50 -0500

    parse positions and velocities

---
+++ source/day10/app.py
@@ -0,0 +1,36 @@
+from parse import parse
+import numpy as np
+
+filename = 'input1'
+filepath = f'data/{filename}.plain'
+
+positions = []
+velocities = []
+
+pattern = 'position=<{px},{py}> velocity=<{vx},{vy}>'
+
+for line in open(filepath):
+    result = parse(pattern, line)
+    position = (int(result['px']), int(result['py']))
+    positions.append(position)
+    velocity = (int(result['vx']), int(result['vy']))
+    velocities.append(velocity)
+
+np_positions = np.array(positions)
+np_velocities = np.array(velocities)
+# new_positions = np_positions + np_velocities
+
+def draw():
+    x_min, y_min = np_positions.min(axis=0)
+    x_max, y_max = np_positions.max(axis=0)
+    for y in range(y_min, y_max + 1):
+        line = ''
+        for x in range(x_min, x_max + 1):
+            if (x, y) in positions:
+                line += '#'
+            else:
+                line += '.'
+        print(line)
+
+draw()
+

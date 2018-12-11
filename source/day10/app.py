100a78eacf0ccc82ae8c7095bceee1159906c0fd
Franco Cedillo <franco.cedillo@gmail.com>
2018-12-10 22:25:02 -0500

    day 10, part 1

--- source/day10/app.py
+++ source/day10/app.py
@@ -1,7 +1,8 @@
 from parse import parse
 import numpy as np
+import pandas as pd
 
-filename = 'input1'
+filename = 'input'
 filepath = f'data/{filename}.plain'
 
 positions = []
@@ -16,13 +17,47 @@ for line in open(filepath):
     velocity = (int(result['vx']), int(result['vy']))
     velocities.append(velocity)
 
-positions_nda = np.array(positions, dtype=tuple)
-velocities_nda = np.array(velocities, dtype=tuple)
+positions_nda = np.array(positions)
+velocities_nda = np.array(velocities)
 
-x_min, y_min = positions_nda.min(axis=0)
-x_max, y_max = positions_nda.max(axis=0)
+
+def gen_dimensions_for_areas(iterations):
+    areas = []
+    for i in range(iterations):
+        current_i_positions_nda = positions_nda + velocities_nda * i 
+        x_min, y_min = current_i_positions_nda.min(axis=0)
+        x_max, y_max = current_i_positions_nda.max(axis=0)
+        w = x_max - x_min
+        h = y_max - y_min
+        a = w * h
+        areas.append(a)
+    return areas
+
+
+def detect_inflection(areas):
+    for i in range(len(areas) - 2):
+        prev_area = areas[i]
+        candidate_area = areas[i+1]
+        next_area = areas[i+2]
+        diff1 = candidate_area - prev_area
+        diff2 = next_area - candidate_area
+        if diff1 * diff2 < 0:
+            return i+1
+
+
+try_iterations = 11000  # after trying with: 10, 20, 100, 1000, 10000, 100000
+
+dimensions_for_areas = gen_dimensions_for_areas(try_iterations)
+
+iterations = detect_inflection(dimensions_for_areas)
+print(f'Inflection i: {iterations}')  # 1027
+
 
 def draw(positions_nda):
+
+    x_min, y_min = positions_nda.min(axis=0)
+    x_max, y_max = positions_nda.max(axis=0)
+    
     for y in range(y_min, y_max + 1):
         line = ''
         for x in range(x_min, x_max + 1):
@@ -32,9 +67,8 @@ def draw(positions_nda):
                 line += '.'
         print(line)
 
-iterations = 4
-for i in range(iterations):
-    print(f'Iteration: {i}')
-    draw(positions_nda)
-    positions_nda += velocities_nda
+
+positions_nda += velocities_nda * iterations
+
+draw(positions_nda)
 

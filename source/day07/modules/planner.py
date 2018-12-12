382c28154f9f694ef5ed4207d8a17a7dd672ccdc
Franco Cedillo <franco.cedillo@gmail.com>
2018-12-12 16:29:42 -0500

    add base_time, basic sort(), backup prints to side comments

--- source/day07/modules/planner.py
+++ source/day07/modules/planner.py
@@ -1,4 +1,4 @@
-def get_time(steps, workers_n):
+def get_time(steps, workers_n, base_time=60):
     """
     Given steps graph and number of workers
     Returns time for work to be completed
@@ -8,25 +8,24 @@ def get_time(steps, workers_n):
 
     # Add amount of work for each node
     for node in steps.nodes:
-        steps.nodes[node]['work'] = ord(node) - ord('A') + 1 + 60
+        steps.nodes[node]['work'] = ord(node) - ord('A') + 1 + base_time
 
     time = 0
     while steps.nodes:
         # Find nodes available for work
+        # If there are 0 edges pointing in to the node
+        # it is, previous steps have been removed
         available_nodes = [node for node in steps.nodes if steps.in_degree(node) == 0]
         
-        # Sort available nodes: Work on nodes with least remaining work
-        available_nodes.sort(key=lambda node: steps.nodes[node]['work'])
+        # Sort available nodes in alphabetical order
+        available_nodes.sort()
         
         # Reduce remaining work for workers_n of the available nodes
         for worker, node in zip(range(workers_n), available_nodes):
-            # print(f'{time}: Worker {worker} is on task {node}')
-            steps.nodes[node]['work'] -= 1
-
+            steps.nodes[node]['work'] -= 1  # print(f'{time}: Worker {worker} is on task {node}')
             # Remove finished nodes
             if steps.nodes[node]['work'] == 0:
-                # print(f'{time}: Node {node} is finished!'))
-                steps.remove_node(node)
+                steps.remove_node(node)  # print(f'{time}: Node {node} is finished!')
 
         # Increase time
         time += 1

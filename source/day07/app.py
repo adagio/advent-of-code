a2ecda3042fabb5ef0bf5775d4d1dbc85d9c538b
Franco Cedillo <franco.cedillo@gmail.com>
2018-12-12 16:33:53 -0500

    use driver

--- source/day07/app.py
+++ source/day07/app.py
@@ -1,30 +1,6 @@
-from parse import parse
-import networkx as nx
-
-from modules.planner import get_time
+from driver import run
 
 
-filename = 'input'
-filepath = f'data/{filename}.plain'
-
-pattern = 'Step {start} must be finished before step {stop} can begin.'
-
-steps = nx.DiGraph()
-
-for line in open(filepath):
-    result = parse(pattern, line)
-    steps.add_edge(result['start'], result['stop'])
-
-gen = nx.lexicographical_topological_sort(steps)
-step_list = list(gen)
-
-steps_string = ''.join(step_list)
-
-print(steps_string)
-
-workers_n = 5
-
-time = get_time(steps, workers_n)
-
-print(f'Finishing all the work took {time} seconds')
+run(filename='input1', workers_n=2, base_time=0)
+run(filename='input', workers_n=5, base_time=60)
 

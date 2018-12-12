46734241689fea2037995b465e70ee5d9dcdf706
Franco Cedillo <franco.cedillo@gmail.com>
2018-12-11 22:48:45 -0500

    remove self prefix from aux data in __init__

--- source/day08/modules/tree.py
+++ source/day08/modules/tree.py
@@ -1,10 +1,10 @@
 class Tree:
 
     def __init__(self, data):
-        self.n_children = data.popleft()
-        self.n_metadata = data.popleft()
-        self.children = [ Tree(data) for _ in range(self.n_children) ]
-        self.metadata = [ data.popleft() for _ in range(self.n_metadata) ]
+        n_children = data.popleft()
+        n_metadata = data.popleft()
+        self.children = [ Tree(data) for _ in range(n_children) ]
+        self.metadata = [ data.popleft() for _ in range(n_metadata) ]
 
     def get_total(self):
         return sum(self.metadata) + sum( child.get_total() for child in self.children )

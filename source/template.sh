2adc2177d57c9ac1191b8fde94da6c5e784ec439
Franco Cedillo <franco.cedillo@gmail.com>
2018-12-18 16:21:32 -0500

    parametrize day

--- source/template.sh
+++ source/template.sh
@@ -1,8 +1,16 @@
-mkdir day07
-mkdir day07/tests
-#touch day06/tests/test_event.py
-mkdir day07/modules
-#touch day06/modules/driver.py
-mkdir day07/data
-touch day07/data/input1.plain
-touch day07/data/input.plain
+#!/bin/bash
+
+day=day13
+echo $day
+mkdir $day
+touch $day/$day.org
+mkdir $day/tests
+#touch $day/tests/test_event.py
+mkdir $day/modules
+#touch $day/modules/driver.py
+mkdir $day/data
+touch $day/data/input1.plain
+touch $day/data/input.plain
+touch $day/app.py
+touch $day/study-app.py
+mkdir $day/study
